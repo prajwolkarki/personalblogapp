034e881561b8dd6b5824098147b241123f864beb
prajwolkarki <prajwolkarki609@gmail.com>
2025-01-26 18:44:52 +0545

    added domain mapping

---
+++ src/lib/emailDomainMap.js
@@ -0,0 +1,9 @@
+const emailDomainMap = {
+    "gmail.com": "https://mail.google.com",
+    "outlook.com": "https://outlook.live.com",
+    "yahoo.com": "https://mail.yahoo.com",
+    "icloud.com": "https://www.icloud.com/mail",
+    "protonmail.com": "https://mail.protonmail.com",
+  };
+
+  export {emailDomainMap};
